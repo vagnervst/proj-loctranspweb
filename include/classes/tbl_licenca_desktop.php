7c7d660bfa69a52bc3a397adb42de672a90f9d7e
Leonardo Ponciano <leonardo@bellets.com>
2017-04-04 11:29:54 -0300

    Criada Pagina de Desktop, funcionamento parcialmente feito

---
+++ include/classes/tbl_licenca_desktop.php
@@ -0,0 +1,45 @@
+<?php
+    namespace Tabela {
+        
+        class LicencaDesktop extends \DB\DatabaseUtils {
+            public static $nome_tabela = "tbl_licencadesktop";
+            public static $primary_key = "id";
+            
+            public $id;
+            public $nome;
+            public $conexoesSimultaneas;
+            public $preco;
+            public $duracaoMeses;
+            
+            public function getTipos($registros_por_pagina = null, $pagina_atual = null, $where = null) {
+                $sql = "SELECT id, nome, conexoesSimultaneas, preco, duracaoMeses";
+                $sql .= "FROM {$this::$nome_tabela} ";
+                
+                 if( !empty($where) ) {
+                    $sql .= " WHERE " . $where;
+                }
+                
+                if( !empty($registros_por_pagina) && !empty($pagina_atual) ) {
+                    $registros_a_ignorar = $registros_por_pagina * ( $pagina_atual - 1 );
+                    
+                    $sql .= " LIMIT " . $registros_por_pagina . " ";
+                    $sql .= "OFFSET " . $registros_a_ignorar;
+                }
+                
+                $resultado = $this->executarQuery( $sql );
+                $resultado = $this->get_array_from_resultado( $resultado );
+                
+                $total_licencas = $this->executarQuery("SELECT COUNT(*) AS total FROM {$this::$nome_tabela}");
+                
+                $info_paginacao = [];
+                $info_paginacao["totalRegistros"] = (int) mysqli_fetch_array( $total_veiculos )[0];
+                $info_paginacao["paginaAtual"] = (int) $pagina_atual;
+                $info_paginacao["registrosPorPagina"] = (int) $registros_por_pagina;
+                
+                $resultado[] = $info_paginacao;
+                
+                return $resultado;
+            }
+        }
+    }
+?>
