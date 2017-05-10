a3d8c3a1392ceee9b669446f904dc053a4e952f2
MatheusOliveiraAmorim <Matheus.Oliveira.Amorim@hotmail.com>
2017-05-10 10:38:52 -0300

    estatisticas no cms

---
+++ CMS/estatisticas_diarias.php
@@ -0,0 +1,43 @@
+<?php
+    require_once("../include/classes/sessao.php");
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>
+            CMS - Estatísticas | City Share
+		</title>
+        <link rel="stylesheet" type="text/css" href="CSS/Style.css">
+        <link rel="icon" href="../img/icones/logoCityShareIcon.png">
+	</head>
+	<body>
+        <div id="container">
+            <?php
+                include("layout/header.php");
+            ?>
+            <div class="CMS_main" id="pag-financeiro">
+                <?php include("layout/menu.php") ?>
+                <div id="box-caminho">
+                    <a href="home.php" class="link-caminho">Home</a> &gt;
+                    <a href="cityshare.php" class="link-caminho">City Share</a> &gt;
+                    <a href="financeiro.php" class="link-caminho">Financeiro</a> &gt;
+                    <a href="estatisticas_cityshare.php" class="link-caminho">Estatísticas</a> &gt;
+                    <a href="estatisticas_diarias.php" class="link-caminho">Valor das diárias</a>
+                </div>
+                <div class="box-conteudo">                
+                    <div class="box-grafico">
+                        <div class="nome-grafico">
+                            Valor das diárias por estado
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+                include("layout/footer.php");
+            ?>
+        </div>      
+	</body>
+</html>
