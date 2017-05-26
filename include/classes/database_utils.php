d512ea69e6dd7b77d770949d907b8a3215d5aba7
Leonardo Ponciano <leonardo@bellets.com>
2017-05-26 05:38:39 -0300

    plano conta

--- include/classes/database_utils.php
+++ include/classes/database_utils.php
@@ -177,7 +177,7 @@
                 $sql = "UPDATE " . $this::$nome_tabela . " ";
                 $sql .= "SET " . $this->get_update_valores($this) . " ";                        
                 $sql .= "WHERE " . $this::$primary_key . " = " . $this->get_valor_primary_key();                                
-                echo $sql;
+                //echo $sql;
                 return $this->executarQuery($sql);
             }
 
@@ -193,7 +193,7 @@
                 $sql = "INSERT INTO " . $this::$nome_tabela . "(" . $this->get_propriedades_preparadas(false) . ") ";
                 $sql .= "VALUES(" . $this->get_valores_preparados(false) . ")";
                 
-                echo $sql;
+                //echo $sql;
                 
                 return $this->executarQuery($sql);
             }                        
